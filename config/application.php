d156ec50d384750ed4032ee9fb48ee3386feadb9
Mike <mikeh@ioprint.com>
2015-09-28 11:09:24 -0400

    application.php

--- config/application.php
+++ config/application.php
@@ -19,7 +19,7 @@ define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
-define( 'NOBLOGREDIRECT', '' );
+// define( 'NOBLOGREDIRECT', '' );
 
 /*
 define('ADMIN_COOKIE_PATH', '/');
