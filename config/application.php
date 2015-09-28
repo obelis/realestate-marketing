e79e5ffdb3844192e8e1e41143c2c81effbdb980
Mike <mikeh@ioprint.com>
2015-09-27 21:57:20 -0400

    application.php

--- config/application.php
+++ config/application.php
@@ -12,7 +12,6 @@ if (file_exists($root_dir . '/.env')) {
 }
 
 /* Multisite */
-/*
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', true); // Set to false if using subdirectories
@@ -25,7 +24,6 @@ define('ADMIN_COOKIE_PATH', '/');
 define('COOKIE_DOMAIN', '');
 define('COOKIEPATH', '');
 define('SITECOOKIEPATH', '');
-*/
 
 /**
  * Set up our global environment constant and load its config first
