7dc529f21c3c41552355560a4e1b54d4b74d3066
Mike <mikeh@ioprint.com>
2015-09-27 21:46:10 -0400

    cookie

--- config/application.php
+++ config/application.php
@@ -20,6 +20,11 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
+define('ADMIN_COOKIE_PATH', '/');
+define('COOKIE_DOMAIN', '');
+define('COOKIEPATH', '');
+define('SITECOOKIEPATH', '');
+
 /**
  * Set up our global environment constant and load its config first
  * Default: development
